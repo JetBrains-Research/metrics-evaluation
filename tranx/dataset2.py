1bd77da54ff776aca23397f75b79f3ea1f4fcdee
Mikhail Evtikhiev <mishaevtikhiev@gmail.com>
2020-08-07 15:41:42 +0300

    Applied suggested changes and added split constant for the dataset split

--- tranx/dataset2.py
+++ tranx/dataset2.py
@@ -42,9 +42,9 @@ class timeout:
 assert astor.__version__ == '0.7.1'
 
 
-def preprocess_conala_dataset(train_file, test_file, grammar_file, src_freq=3, code_freq=3,
+def preprocess_conala_dataset(train_file, test_file, grammar_file, split, src_freq=3, code_freq=3,
                               mined_data_file=None, api_data_file=None,
-                              vocab_size=20000, num_mined=0, out_dir='data/conala', n_jobs=1):
+                              vocab_size=20000, num_mined=0, out_dir='data/conala', n_jobs=1, n_files=20): 
     np.random.seed(1234)
 
     asdl_text = open(grammar_file).read()
@@ -55,11 +55,11 @@ def preprocess_conala_dataset(train_file, test_file, grammar_file, src_freq=3, c
     train_examples = preprocess_dataset(train_file, name='train',
                                         transition_system=transition_system, n_jobs=n_jobs)
 
-    # held out 200 examples for development
-    full_train_examples = train_examples[:]
+    # held out (1-split)*len(train_examples) examples for development
+    #full_train_examples = train_examples[:]
     # np.random.shuffle(train_examples)
-    dev_examples = train_examples[-2175:]
-    train_examples = train_examples[:-2175]
+    dev_examples = train_examples[int(split*len(train_examples)):]
+    train_examples = train_examples[:int(split*len(train_examples))]
 
     mined_examples = []
     api_examples = []
@@ -116,10 +116,16 @@ def preprocess_conala_dataset(train_file, test_file, grammar_file, src_freq=3, c
     print('Avg action len: %d' % np.average(action_lens), file=sys.stderr)
     print('Actions larger than 100: %d' % len(list(filter(lambda x: x > 100, action_lens))),
           file=sys.stderr)
-
-    pickle.dump(train_examples,
-                open(os.path.join(out_dir, 'train.all_{}.bin'.format(num_mined)), 'wb'))
-    pickle.dump(full_train_examples, open(os.path.join(out_dir, 'train.gold.full.bin'), 'wb'))
+    #dump_size = int(len(train_examples)/n_files)
+    train_split = np.array_split(np.array(train_examples), n_files)
+    for i, train_out in enumerate(train_split):
+        pickle.dump(list(train_out),open(os.path.join(out_dir, f'train.all_{i}.bin'), 'wb'))
+#    for i in range(n_files-1):
+#        pickle.dump(train_examples[i * dump_size: (i+1) * dump_size],
+#                open(os.path.join(out_dir, 'train.all_{}.bin'.format(i)), 'wb'))
+#    pickle.dump(train_examples[(n_files-1) * dump_size:],
+#            open(os.path.join(out_dir, 'train.all_{}.bin'.format(n_files-1)), 'wb'))
+    #pickle.dump(full_train_examples, open(os.path.join(out_dir, 'train.gold.full.bin'), 'wb'))
     pickle.dump(dev_examples, open(os.path.join(out_dir, 'dev.bin'), 'wb'))
     pickle.dump(test_examples, open(os.path.join(out_dir, 'test.bin'), 'wb'))
     if mined_examples and api_examples:
@@ -266,12 +272,16 @@ if __name__ == '__main__':
                             help='First k number from pretrain file')
     arg_parser.add_argument('--include_api', type=str, help='Path to apidocs file')
     arg_parser.add_argument('--n_jobs', type=int, help='Number of threads to run preprocessing')
+    arg_parser.add_argument('--n_files', type=int, default=40, help='Number of files to dump the train output in')
+    arg_parser.add_argument('--train_file', type=str, default='docstrings-train.json', help='JSON file with train dataset')
+    arg_parser.add_argument('--test_file', type=str, default='docstrings-test.json', help='JSON file with test dataset')
+    arg_parser.add_argument('--split', type=float, default=0.97959, help='Ratio between train and dev parts') #0.97959 corresponds to the 0.96/(0.96+0.02) split we chose
     args = arg_parser.parse_args()
     print(os.getcwd())
 
     # the json files can be downloaded from http://conala-corpus.github.io
-    preprocess_conala_dataset(train_file='docstrings-train.json',
-                              test_file='docstrings-test.json',
+    preprocess_conala_dataset(train_file=args.train_file,
+                              test_file=args.test_file,
                               mined_data_file=args.pretrain,
                               api_data_file=args.include_api,
                               grammar_file='asdl/lang/py3/py3_asdl.simplified.txt',
@@ -279,4 +289,6 @@ if __name__ == '__main__':
                               vocab_size=args.vocabsize,
                               num_mined=args.topk,
                               out_dir=args.out_dir,
-                              n_jobs=args.n_jobs)
+                              n_jobs=args.n_jobs,
+                              n_files=args.n_files,
+                              split=args.split)
