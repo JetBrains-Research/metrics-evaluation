6b77538d832c8a8881eb66cd4a80d28ca4a7a5d8
Mikhail Evtikhiev <mishaevtikhiev@gmail.com>
2021-05-27 19:59:54 +0300

    update grading tools

--- hs-rate.py
+++ hs-rate.py
@@ -5,6 +5,14 @@ import os
 import random
 import tkinter as tk
 
+def check_experience(s):
+    if (s.isdigit() == False):
+        return False
+    exp = int(s)
+    if (exp > 30):
+        return False
+    return True
+
 filename='./to-grade/hs.json'
 try:
     with open(filename[:-5]+'.tmp.json') as f:
@@ -12,7 +20,7 @@ try:
 except:
     with open(filename) as f:
         dat = json.load(f)
-mylist = [(x, y) for x in range(len(dat)) for y in range(3)]
+mylist = [(x, y) for x in range(len(dat)-1) for y in range(3)]
 random.shuffle(mylist)
 names = ('gcnn', 'nl2code', 'snippet')
 
@@ -54,9 +62,38 @@ image_viewer_column = [
 
 # ----- Full layout -----
 
-layout = [
+layout_form = [[sg.Text('''Dear participant,\n
+this program is a survey on quality of the code snippets conducted by Independent non-profit organization of additional professional education 
+“Research and Education Center “JetBrains”, OGRN 1187800000134, located at St. Petersburg, Kantemirovskaya street 2, liter A, office 201. 
+You will be presented with code snippets (one at a time) and a problem they are supposed to solve. You are asked to evaluate whether 
+the suggested snippet is helpful or not helpful in solving the problem on a scale from 0 to 4, where 0 corresponds to a totally irrelevant snippet 
+and 4 corresponds to a snippet which solves the problem (more detailed instruction will be present at the snippet grading screen).\n
+In the event of any publication or presentation resulting from the research, no personally identifiable information will be shared.
+We plan to include the results of this survey in a scientific publication. If you have any concerns or questions about your rights as a participant 
+or about the way the study is being conducted, please contact Mikhail Evtikhiev (mikhail.evtikhiev@jetbrains.com).''', font=("Helvetica", 12))],
 
-    [
+    [sg.Text('''In the text box below please write, for how long have you been programming in Python (in years), 
+rounded to the nearest integer number. This information will be reported in the publication in an aggregated form.''')],
+
+    [sg.Text('Python experience: ', key = '_text1_'), sg.InputText(key='_python_', size=(10, 1))],
+
+    [sg.Text('''In the text box below please write your Slack handle or e-mail address. This information will be kept private and we only ask for it 
+to be able to reach back to you to clarify any technical uncertainties with the graded snippets, if such uncertainties shall arise.''')],
+
+    [sg.Text('Contact information: ', key = '_text2_'), sg.InputText(key='_contact_', size=(30, 1))],
+
+    [sg.Text('''ELECTRONIC CONSENT\n
+Please select your choice below. Selecting the “yes” option below indicates that: 
+i) you have read and understood the above information, 
+ii) you voluntarily agree to participate, and 
+iii) you are at least 18 years old. 
+If you do not wish to participate in the research study, please decline participation by selecting “No”.''')],
+
+    [sg.Ok(button_text="Yes"), sg.Exit(button_text="No")],
+
+    ]
+
+layout_grade = [[
 
         sg.Column(file_list_column),
 
@@ -65,17 +102,41 @@ layout = [
         sg.Column(image_viewer_column),
 
     ]
-
 ]
+
 layout1 = [[sg.Text('')]]
 root = tk.Tk()
 screen_width = root.winfo_screenwidth()
 scaling_window = sg.Window('Window Title', layout1, no_titlebar=True, auto_close=False, alpha_channel=0).Finalize()
 scaling_window.TKroot.tk.call('tk', 'scaling', max(screen_width/1920,1))
 scaling_window.close()
-window = sg.Window("Hearthstone dataset grader", layout, finalize=True, location=(0,0), return_keyboard_events=True)
 
+pers_data = dat[-1]
+if ((pers_data["contact"] == "") or (pers_data["experience"] == "") or (pers_data["consent"] == "")):
+    window = sg.Window("Hearthstone dataset grader form", layout_form, finalize=True, location=(0,0), return_keyboard_events=True)
+    no_consent = True
+    while (no_consent):
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            window.close()
+        elif event == "Ok":
+            error_text = ""
+            if (check_experience(values['_python_']) == False):
+                error_text += "Incorrect input. Please enter, for how long have you been programming in Python (in years, rounded to a nearest integer)\n"
+            if (values['_contact_'].length() < 1):
+                print('Incorrect input. Please enter your Slack handle or e-mail address.\n')
+            if len(error_text) > 0:
+                sg.popup(error_text)
+            else:
+                pers_data["contact"] = values['_contact_']
+                pers_data["experience"] = int(values['_python_'])
+                pers_data["consent"] = 'yes'
+                no_consent = False
+                for key in dat[-1]:
+                    dat[-1][key] = pers_data[key]
 
+window = sg.Window("Hearthstone dataset grader", layout_grade, finalize=True, location=(0,0), return_keyboard_events=True)
+if no_consent: window.close()
 # Run the Event Loop
 for (i, j) in mylist:
     successful = False
--- rate.py
+++ rate.py
@@ -2,6 +2,7 @@ import click
 import json
 import os
 import random
+import sys
 from pygments import highlight
 from pygments.lexers.python import PythonLexer
 from pygments.formatters.terminal import TerminalFormatter
@@ -14,8 +15,69 @@ gauge = (1, 5, 49, 70, 91, 93, 97, 110, 114, 127, 136, 151, 171, 194, 202, 213, 
 # gauge is the list of 40 intents with 40*5 = 200 snippets to be graded by all our graders. These will allow to compare the graders and yield better estimate for the other grades
 names = ('baseline', 'tranx-annot', 'best-tranx', 'best-tranx-rerank', 'snippet')
 
+def get_exp():
+	while True:
+		exp_str = input()
+		try:
+			experience = int(exp_str)
+			if (0 > experience) or (experience > 30): 
+				raise ValueError
+			else:
+				return experience
+		except ValueError:
+			print('Incorrect input. Please enter, for how long have you been programming in Python (in years, rounded to a nearest integer)')
+
+def get_name():
+	while True:
+		name = input()
+		if (len(name) < 1): 
+			print('Incorrect input. Please enter your Slack handle or e-mail address.')
+		else:
+			return name
+
+def get_consent():
+	while (True):
+		consent = input().lower()
+		if (consent == 'yes'):
+			return
+		elif (consent == 'no'):
+			sys.exit()
+		else:
+			print('Incorrect input. Please enter "yes", if you wish to participate in the study, and "no", if you want to decline participation')		
+
+
+def consent():
+	print('''Dear participant,\n
+this program is a survey on quality of the code snippets conducted by Independent non-profit organization of additional professional education 
+“Research and Education Center “JetBrains”, OGRN 1187800000134, located at St. Petersburg, Kantemirovskaya street 2, liter A, office 201. 
+You will be presented with code snippets (one at a time) and a problem they are supposed to solve. You are asked to evaluate 
+whether the suggested snippet is helpful or not helpful in solving the problem on a scale from 0 to 4, 
+where 0 corresponds to a totally irrelevant snippet and 4 corresponds to a snippet which solves the problem 
+(more detailed instruction will be present at the snippet grading screen).
+In the event of any publication or presentation resulting from the research, no personally identifiable information will be shared.
+We plan to include the results of this survey in a scientific publication. If you have any concerns or questions about your rights
+as a participant or about the way the study is being conducted, please contact Mikhail Evtikhiev (mikhail.evtikhiev@jetbrains.com).''')
+	print(' ')
+	print('''Please write, for how long have you been programming in Python (in years), 
+rounded to the nearest integer number. This information will be reported in the publication in an aggregated form.''')
+	pers_data = dict()
+	pers_data["experience"] = get_exp()
+	print('''Please write your Slack handle or e-mail address. This information will be kept private and we only ask for it to be able to reach back to you 
+to clarify any technical uncertainties with the graded snippets, if such uncertainties shall arise.''')
+	pers_data["contact"] = input()
+	print('ELECTRONIC CONSENT\n')
+	print('''Please enter your choice below. Entering the “yes” option below indicates that:\n
+i) you have read and understood the above information,\n
+ii) you voluntarily agree to participate, and \n
+iii) you are at least 18 years old.\n
+If you do not wish to participate in the research study, please decline participation by entering "no"''')
+	get_consent()
+	pers_data["consent"] = 'yes'
+	return pers_data
+
 @click.command()
 @click.option('--filename', default='./to-grade/all-singles.json', help='JSON dataset of code snippets to be rated')
+
 def loadprint(filename):
 	try:
 		with open(filename[:-5]+'.tmp.json') as f:
@@ -24,11 +86,16 @@ def loadprint(filename):
 		with open(filename) as f:
 			dat = json.load(f)
 	mylist = [(x, y) for x in gauge for y in range(5)]
-	testlist = [(x, y) for x in range(len(dat)) if x not in gauge for y in range(5)]
+	testlist = [(x, y) for x in range(len(dat)-1) if x not in gauge for y in range(5)]
 	random.shuffle(testlist)
 	random.shuffle(mylist)
 	mylist.extend(testlist)
 	cnt = 0
+	pers_data = dat[-1]
+	if ((pers_data["contact"] == "") or (pers_data["experience"] == "") or (pers_data["consent"] == "")):
+		pers_data = consent()
+		for key in dat[-1]:
+			dat[-1][key] = pers_data[key]
 	for (i, j) in mylist:
 		sname = 'grade-' + names[j]
 		if sname in dat[i]:
